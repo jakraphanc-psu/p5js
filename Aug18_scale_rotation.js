019e575c6ceb6fbf1f18277775b753966772a606
jakraphanc-psu <jakraphan.c@phuket.psu.ac.th>
2025-08-18 15:00:34 +0700

    Update Aug18_scale_rotation.js

--- Aug18_scale_rotation.js
+++ Aug18_scale_rotation.js
@@ -1,5 +1,6 @@
 let c_fill = ['rgb(11,241,66)','rgb(7,4,255)','rgb(250,253,1)'];
 
+
 function setup() {
   createCanvas(600, 600);
   noLoop();
@@ -8,27 +9,36 @@ function draw() {
   background('red');
   for(i=0;i<100;i++){
     a = round(random(0,c_fill.length-1));
-    var scalar = random(0.25, 1.0);
-    var angle = random(0,360);
-    owl(random(0,600),random(0,600),c_fill[a],scalar,angle); 
+    owl(random(0,600),random(0,600),c_fill[a]);
   }
+  
 }
-function owl(x, y, c,s,r) {
+function owl(x, y, c) {
   push();
   translate(x, y);
-  rotate(r);
-  scale(s);
+  scale(random(0,1)); 
+  rotate(random(0,360));
   stroke(c);
   strokeWeight(70);
   line(0, -35, 0, -65); // Body
+  stroke(0);
+  strokeWeight(2);
+  line(-25,-75,25,-75); 
+  fill(c);
+  circle(-50,-20,10);
   noStroke();
-  fill(255);
-  ellipse(-17.5, -65, 35, 35); // Left eye dome
-  ellipse(17.5, -65, 35, 35); // Right eye dome
-  arc(0, -65, 70, 70, 0, PI); // Chin
+  fill('pink');
+  rect(-25,-100,50,50);
+  fill('rgb(183,24,255)');
+  circle(50,-100,50);
+  fill('rgb(24,255,206)');
+  circle(-50,-100,50);
   fill(0);
-  ellipse(-14, -65, 8, 8); // Left eye
-  ellipse(14, -65, 8, 8); // Right eye
-  quad(0, -58, 4, -51, 0, -44, -4, -51); // Beak
+  circle(random(-10,10),random(-50,50),random(5,30));
+  stroke(0);
+  strokeWeight(1);
+  line(-75,-100,75,-100);
+  fill('rgba(255,255,0,0.5)');
+  circle(0,0,100);
   pop();
 }
